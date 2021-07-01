a281f85fe37cc963c464077cb77b75e4cda1db7f
nanhson <son.nguyen@fdssoft.com>
2021-07-02 00:39:10 +0700

    [feature-add-HD-wallet] Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
+const { NetworkType } = require('symbol-sdk');
 const centiva = require('./lib/cjs/index');
+const SimpleWallet = centiva.SimpleWallet;
 
 run();
+runHD();
 
 async function run() {
     /**
@@ -10,15 +13,23 @@ async function run() {
     // console.log("Generated account's address: ", acc.address.address);
     // console.log("Generated account's privKey: ", acc.privateKey.toString());
 
+    // const pk = acc.privateKey.toString();
+    // const pklc = pk.toLowerCase();
+    // console.log('pklc: ', pklc);
+
+    // const accCHeck = centiva.AccountUtil.generateNewAccountWithPrivateKey(pklc, 152);
+    // console.log("Generated account's address: ", accCHeck.address.address);
+    // console.log("Generated account's privKey: ", accCHeck.privateKey.toString());
+
     /**
      * Get Account info
      */
     // const result = await centiva.AccountUtil.getAccountInfo('TBEFN3SSXFFEIUOJQLXSZBRJGN56G4XHW647OQQ');
     // console.log(result);
-    centiva.AccountUtil.getMosaicSent({
-        recipientRawAddress: 'TBEFN3SSXFFEIUOJQLXSZBRJGN56G4XHW647OQQ',
-        mosaicIdHex: '5A4935C1D66E6AC4'
-    });
+    // centiva.AccountUtil.getMosaicSent({
+    //     recipientRawAddress: 'TBEFN3SSXFFEIUOJQLXSZBRJGN56G4XHW647OQQ',
+    //     mosaicIdHex: '5A4935C1D66E6AC4'
+    // });
     // centiva.AccountUtil.getMosaicSent({
     //     nodeUrl: 'http://61.27.29.85:3000',
     //     recipientRawAddress: 'TDG7K4QTI4Z6BDVM7LI2OWMCBS6IA5IKKHXXCGY',
@@ -67,4 +78,68 @@ async function run() {
     //     6,
     //     100000000
     // )
+}
+
+function runHD() {
+    // var base32 = require('hi-base32');
+    // const hdwallet = require('symbol-hd-wallets');
+    // const MnemonicPassPhrase = hdwallet.MnemonicPassPhrase;
+
+    // const mnemonic = MnemonicPassPhrase.createRandom();
+    // const secureSeedHex = mnemonic.toSeed('your-password');
+    
+    // console.log('mnemonic: ', mnemonic);
+    // console.log('secureSeedHex: ', secureSeedHex.toString('hex'));
+
+    // examples/GeneratingAHDWalletPublicNetworkCompatible.ts
+
+    /** */
+    // const Network = require("symbol-hd-wallets").Network;
+    // const NetworkType = require("symbol-sdk").NetworkType;
+    // const Wallet = require("symbol-hd-wallets").Wallet;
+    // const ExtendedKey = require("symbol-hd-wallets").ExtendedKey;
+
+    // const xkey = ExtendedKey.createFromSeed('000102030405060708090a0b0c0d0e0f', Network.SYMBOL);
+    // const wallet = new Wallet(xkey);
+
+    // // get master account
+    // const masterAccountPK = wallet.getAccountPrivateKey();
+    // console.log('masterAccountPK: ', masterAccountPK);
+
+    // // get DEFAULT ACCOUNT
+    // const defaultAccount = wallet.getChildAccountPrivateKey();
+    // console.log('defaultAccount: ', defaultAccount);
+
+    // // derive specific child path
+    // const childAccount = wallet.getChildAccountPrivateKey('m/44\'/4343\'/0\'/0\'/0\'', NetworkType.TEST_NET);
+    // console.log('childAccount: ', childAccount);
+
+    // get read-only wallet
+    // const readOnlyWallet = new Wallet(xkey.getPublicNode());
+    // const readOnlyAccount = readOnlyWallet.getPublicAccount(NetworkType.TEST_NET);
+
+    // get read-only DEFAULT ACCOUNT
+    // const readOnlyDefaultAccount = readOnlyWallet.getChildPublicAccount();
+
+    // const mnemonic = centiva.AccountUtil.generateHDWalletMnemonic();
+    // console.log(mnemonic.plain);
+
+    // const seed = centiva.AccountUtil.generateProtectedSeedFromMnemonic(mnemonic.plain);
+    // console.log('seed: ', seed);
+    // const wallet = centiva.AccountUtil.getHDWalletFromMnemonic(mnemonic.plain);
+    // const account0 = centiva.AccountUtil.getAccountAtIndex(wallet, 0, NetworkType.TEST_NET);
+    // console.log('account privkey: ', account0.privateKey);
+
+    // const sw = SimpleWallet.createFromPrivateKey('name', new centiva.Password('37777777'), account0.privateKey, centiva.NetworkType.TEST_NET);
+    // console.log('sw.toDTO(): ', sw.toDTO());
+    // console.log('base64 DTO: ', Buffer.from(JSON.stringify(sw.toDTO())).toString('base64'));
+
+    // const nw = 104;
+    // const createdWallet = SimpleWallet.createFromPrivateKey(
+    //     'HIT',
+    //     new centiva.Password('37777777'),
+    //     account0.privateKey,
+    //     nw.valueOf()
+    // );
+    // console.log(createdWallet.toDTO());
 }
